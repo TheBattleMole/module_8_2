49bd95aab428e0c9b84391842152af4d413888ac
Aleksey_Abramenko <x903xxxx@gmail.com>
2025-01-20 19:54:21 +0300

    Add files via upload

--- module_8_2.py
+++ module_8_2.py
@@ -23,16 +23,16 @@ def calculate_average(numbers):
             if type(i) == int:
                 kol.append(i)
     except TypeError:
-            pass
-
+            print("В numbers записан некорректный тип данных")
+            
     try:
         average = personal_sum(numbers)[0] / len(kol)
     except ZeroDivisionError:
         average = 0
     return average
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+# print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+# print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+# print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
 
